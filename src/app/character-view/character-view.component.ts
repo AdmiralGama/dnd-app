b689e27179f19d5200ff22b2cd0bc4a6e649355d
Admiral Gama <cposlund@gmail.com>
2024-05-27 20:31:01 -0700

    UI Start

--- src/app/character-view/character-view.component.ts
+++ src/app/character-view/character-view.component.ts
@@ -19,6 +19,138 @@ import { Character } from '../character';
     <p>{{ character?.id }}</p>
     <p>{{ character?.name }}</p>
     <p>{{ character?.stats }}</p>
+    <section class="border border-dark" style="margin: 5% 12%; padding: 5%">
+      <!--Header-->
+      <div class="border border-dark row">
+        <!--Name-->
+        <div style="width: 40%">
+          <h3 style="text-align: center;">Character Name</h3>
+        </div>
+        <!--Info-->
+        <div style="width: 60%;">
+          <div class="row">
+            <div class="col">
+              <p style="text-align: center;">Class & Level</p>
+              <p style="text-align: center;">Race</p>
+            </div>
+            <div class="col">
+              <p style="text-align: center;">Background</p>
+              <p style="text-align: center;">Alignment</p>
+            </div>
+            <div class="col">
+              <p style="text-align: center;">Player Name</p>
+              <p style="text-align: center;">Experience</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--Main Content-->
+      <div class="row">
+        <!--Left Column-->
+        <div class="border border-dark col" style="padding: 2%;">
+          <!--Stats-->
+          <div class="row">
+            <!--Base Stats-->
+            <div class="col" style="width: 40%;">
+              <div class="border border-dark" style="margin: 5%; width: 95%;">
+                <h3 style="text-align: center;">Str</h3>
+              </div>
+              <div class="border border-dark" style="margin: 5%; width: 95%;">
+                <h3 style="text-align: center;">Dex</h3>
+              </div>
+              <div class="border border-dark" style="margin: 5%; width: 95%;">
+                <h3 style="text-align: center;">Con</h3>
+              </div>
+              <div class="border border-dark" style="margin: 5%; width: 95%;">
+                <h3 style="text-align: center;">Int</h3>
+              </div>
+              <div class="border border-dark" style="margin: 5%; width: 95%;">
+                <h3 style="text-align: center;">Wis</h3>
+              </div>
+              <div class="border border-dark" style="margin: 5%; width: 95%;">
+                <h3 style="text-align: center;">Cha</h3>
+              </div>
+            </div>
+
+            <!--Proficiencies-->
+            <div class="border border-dark col" style="width: 60%;">
+              <div class="border border-dark">
+                <p style="text-align: center;">Proficiency Bonus</p>
+              </div>
+              <div class="border border-dark">
+                <p style="text-align: center;">Saving Throws</p>
+              </div>
+              <div class="border border-dark">
+                <p style="text-align: center;">Skills</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="border border-dark">
+            <p style="text-align: center;">Passive Wisdom</p>
+          </div>
+          <div class="border border-dark">
+            <p style="text-align: center;">Other Proficiencies & Languages</p>
+          </div>
+        </div>
+        <!--Middle Column-->
+        <div class="border border-dark col" style="padding: 2%;">
+          <div class="row">
+            <div class="border border-dark col">
+              <p style="text-align: center;">Armor Class</p>
+            </div>
+            <div class="border border-dark col">
+              <p style="text-align: center;">Initiative</p>
+            </div>
+            <div class="border border-dark col">
+              <p style="text-align: center;">Speed</p>
+            </div>
+          </div>
+
+          <div class="border border-dark">
+            <p style="text-align: center;">Current HP</p>
+          </div>
+          <div class="border border-dark">
+            <p style="text-align: center;">Temp HP</p>
+          </div>
+
+          <div class="row">
+            <div class="border border-dark col">
+              <p style="text-align: center;">Hit Dice</p>
+            </div>
+            <div class="border border-dark col">
+            <p style="text-align: center;">Death Saves</p>
+            </div>
+          </div>
+
+          <div class="border border-dark">
+            <p style="text-align: center;">Attacks & Spellcasting</p>
+          </div>
+          <div class="border border-dark">
+            <p style="text-align: center;">Equipment</p>
+          </div>
+        </div>
+        <!--Right Column-->
+        <div class="border border-dark col" style="padding: 2%;">
+          <p style="text-align: center;">Column</p>
+          <div class="border border-dark">
+            <p style="text-align: center;">Personality Traits</p>
+          </div>
+          <div class="border border-dark">
+            <p style="text-align: center;">Ideals</p>
+          </div>
+          <div class="border border-dark">
+            <p style="text-align: center;">Bonds</p>
+          </div>
+          <div class="border border-dark">
+            <p style="text-align: center;">Flaws</p>
+          </div>
+          <div class="border border-dark">
+            <p style="text-align: center;">Features & Traits</p>
+          </div>
+        </div>
+      </div>
+    </section>
   `,
   styleUrl: './character-view.component.css'
 })
